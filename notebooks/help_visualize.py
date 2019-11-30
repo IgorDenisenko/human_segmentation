241d7bcd1ce9ff3cce4dc2baf74e1f3809ad6810
Igor <Deni130497@yandex.ru>
2019-11-30 03:43:16 +0300

    hum seg

---
+++ notebooks/help_visualize.py
@@ -0,0 +1,17 @@
+import os
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+
+def visualize(**images):
+    """PLot images in one row."""
+    n = len(images)
+    plt.figure(figsize=(16, 5))
+    for i, (name, image) in enumerate(images.items()):
+        plt.subplot(1, n, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(' '.join(name.split('_')).title())
+        plt.imshow(image)
+    plt.show()
